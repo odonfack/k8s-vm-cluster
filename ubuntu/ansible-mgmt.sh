95d5d92d14b1ad3012b045df8a648ecbade3b8f4
Donfack <Olivier.Donfack@computacenter.com>
2024-04-08 15:28:42 +0200

    update inventory

--- ubuntu/ansible-mgmt.sh
+++ ubuntu/ansible-mgmt.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-sed -i -e 's/#DNS=/DNS=8.8.8.8/' /etc/systemd/resolved.conf
-
-service systemd-resolved restart
-
-sudo su - ubuntu
-sudo apt update
-sudo apt install -y git
-sudo apt install -y software-properties-common
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible
-ansible-galaxy collection install ansible.posix
-exit
+# sudo su - ubuntu
+# sudo apt update
+# sudo apt install -y git
+# sudo apt install -y software-properties-common
+# sudo add-apt-repository --yes --update ppa:ansible/ansible
+# sudo apt install -y ansible
+# ansible-galaxy collection install ansible.posix
+# exit
