add1b3de87c2ad1a4b36ea7db34b9ea911990a49
Donfack <Olivier.Donfack@computacenter.com>
2024-04-08 17:19:46 +0200

    update inventory

--- ansible_playbooks/join_cluster.sh
+++ ansible_playbooks/join_cluster.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.111:6443 --token gyyt7j.515m4ks28tvltm3x --discovery-token-ca-cert-hash sha256:a0fc4e2311452e45909f00d2b4059afce5bafbe4ea22eac716858f46e14ced46 
+kubeadm join 192.168.0.241:6443 --token gyyt7j.515m4ks28tvltm3x --discovery-token-ca-cert-hash sha256:a0fc4e2311452e45909f00d2b4059afce5bafbe4ea22eac716858f46e14ced46 
