03f7ba9d21f16257cdf7d9bbdae176f388f8bb12
Ubuntu <ubuntu@kubemgmt1>
2024-04-10 12:59:41 +0000

    add metallb und ingree

--- ansible_playbooks/join_cluster.sh
+++ ansible_playbooks/join_cluster.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.111:6443 --token gyyt7j.515m4ks28tvltm3x --discovery-token-ca-cert-hash sha256:a0fc4e2311452e45909f00d2b4059afce5bafbe4ea22eac716858f46e14ced46 
+kubeadm join 192.168.0.241:6443 --token kb76y2.u6o2zb6eqbjp9bud --discovery-token-ca-cert-hash sha256:6c54e1ce65da2a1b46960595d691ebca042347c0fa34ab84b7a733117abefe79 
