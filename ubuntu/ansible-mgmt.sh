ff210a0fea277e173efb170eae30c78d197f273d
Donfack <Olivier.Donfack@computacenter.com>
2024-10-31 22:03:04 +0100

    update ansible mgnt

--- ubuntu/ansible-mgmt.sh
+++ ubuntu/ansible-mgmt.sh
@@ -3,7 +3,7 @@
 sudo su - ubuntu
 sudo apt update
 sudo apt install -y git
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.28.8/bin/linux/amd64/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.31.0/bin/linux/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin
 UBUNTU_CODENAME=focal
@@ -11,4 +11,7 @@ wget -O- "https://keyserver.ubuntu.com/pks/lookup?fingerprint=on&op=get&search=0
 echo "deb [signed-by=/usr/share/keyrings/ansible-archive-keyring.gpg] http://ppa.launchpad.net/ansible/ansible/ubuntu $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/ansible.list
 sudo apt -y update && sudo apt -y install ansible
 ansible-galaxy collection install ansible.posix
+wget https://get.helm.sh/helm-v3.12.3-linux-amd64.tar.gz
+tar -xvf  helm-v3.12.3-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/helm
 exit
